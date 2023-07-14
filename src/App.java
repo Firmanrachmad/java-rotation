e3779f51a65d9a347fa8d7ecd95b3855ff6bcef2
Firmanrachmad <dragobniel@gmail.com>
2023-07-14 20:56:24 +0700

    completed

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void rotateArray(int[] arr, int count) {
+        int[] rotatedArray = new int[arr.length];
+        
+        for (int i=0;i<arr.length;i++){
+            int newIndex = (i + count) % arr.length;
+            rotatedArray[newIndex] = arr[i];
+        }
+        
+        for (int i=0;i<arr.length;i++){
+            System.out.print(rotatedArray[i]+"-");
+        }
+        
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+        int[] num = new int[5];
+        int count = 0;
+        
+        for (int i=0;i<num.length;i++){
+            System.out.print("Input Numbers = ");
+            num[i] = sc.nextInt();
+        }
+        
+        for (int i=0;i<num.length;i++){
+            System.out.print(num[i]+"-");
+        }
+        System.out.println("");
+        System.out.print("Input Count Rotation = ");
+        count = sc1.nextInt();
+        
+        rotateArray(num, count);
+        
     }
 }
